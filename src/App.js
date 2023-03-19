a8e767b61664184f0a6fd3d5d7bb5b0a75759f0f
Tridip <chavdatridip007@gmail.com>
2023-03-19 09:29:34 +0530

    Add Log In Sign In

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
 import logo from './logo.svg';
 import './App.css';
+import { useContext, useEffect, useImperativeHandle, useState } from 'react';
+import axios, { Axios } from 'axios';
+import { Navbar } from './Components/Navbar';
+import { SearchBar } from './Components/Search';
+import { BookCard } from './Components/BookCard';
+import React from "react";
+import BookState from './Context/BookDetails';
+import { Login } from './Components/Login';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { RegisterForm } from './Components/RegisterForm';
+
 
 function App() {
+
   return (
+    <BookState>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    
+    <Navbar/>
+
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Login/>}/>
+            <Route path="/store" element={<><SearchBar/><BookCard/></>} />
+            <Route path="/register" element={<RegisterForm/>} />
+        </Routes>
+      </BrowserRouter>
+      
     </div>
+    </BookState>
+    
   );
 }
 
